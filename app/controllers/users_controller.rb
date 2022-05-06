e1ea169df5ac8b0aa059f3f1d1759d7dc18c0792
EC2 Default User <ec2-user@ip-172-31-40-6.ec2.internal>
2022-05-06 14:56:59 +0000

    [ADD]ユーザー機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params:[id])
+    @books = @user.books
   end
 
   def edit
+    @user = User.find(params:[id])
   end
 
   def index
+    @users= User.all
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :introduction, :profile_image)
   end
 end
