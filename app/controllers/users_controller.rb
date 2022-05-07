88052d44298725b67b2c38426e224dec777d92b5
EC2 Default User <ec2-user@ip-172-31-40-6.ec2.internal>
2022-05-07 09:33:47 +0000

    [ADD]全viewページの実装、Book＆Userの編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,29 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params:[id])
+    @user = User.find(params[:id])
     @books = @user.books
   end
 
   def edit
-    @user = User.find(params:[id])
+    @user = User.find(params[:id])
   end
 
   def index
-    @users= User.all
+    @users = User.all
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render :edit
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:user_name, :introduction, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
