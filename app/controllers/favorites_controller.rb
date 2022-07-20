0d6312ac5c01c472d437eb04cea1147d91668041
EC2 Default User <ec2-user@ip-172-31-40-6.ec2.internal>
2022-07-20 09:04:28 +0000

    [Add]flash notice

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,17 @@
 class FavoritesController < ApplicationController
-  def create
-    @book = Book.find(params[:book_id])
-    @favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
-    @favorite.save
-    redirect_to request.referer
-  end
+ def create
+  @books = Book.all
+  @book = Book.find_by(params[:book_id])
+  @favorite = Favorite.new(user_id: current_user.id, book_id: @book.id)
+  @favorite.save
+  render 'replace_btn'
+ end
 
-  def destroy
-    @book = Book.find(params[:book_id])
-    @favorite = Favorite.find_by(user_id: current_user.id, book_id: params[:book_id])
-    @favorite.destroy
-    redirect_to request.referer
-  end
+ def destroy
+  @book = Book.find_by(params[:book_id])
+  @favorite = Favorite.find_by(user_id: current_user.id, book_id: @book.id)
+  @favorite.destroy
+  @books = Book.all
+  render 'replace_btn'
+ end
 end
