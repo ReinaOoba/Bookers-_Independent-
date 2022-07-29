c3b607602bc248243b2353e477b1b24a88fed332
EC2 Default User <ec2-user@ip-172-31-40-6.ec2.internal>
2022-07-29 04:30:30 +0000

    [ADD]validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
-  validates :email,        uniqueness: true
+  validates :email,        presense: true,uniqueness: true
   validates :name,         presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum:50 }
 
