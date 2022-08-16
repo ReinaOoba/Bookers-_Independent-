cafee0aeebdb3a0176c8c58128d2c9a25bb59382
EC2 Default User <ec2-user@ip-172-31-40-6.ec2.internal>
2022-08-16 23:42:57 +0000

    [Fix]Argument Error

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
-  validates :email,        presense: true,uniqueness: true
+  validates :email,        presence: true, uniqueness: true
   validates :name,         presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum:50 }
 
